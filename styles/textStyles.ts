4dda2d2a401adb96330b1179e92510e32ed8193d
Breno Prestes <bcaua205@gmail.com>
2023-11-19 19:53:03 -0300

    Add general styles

---
+++ styles/textStyles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const textStyles = StyleSheet.create({
+    title: {
+        fontFamily: 'Roboto',
+        fontSize: 25,
+        fontWeight: '700'
+    },
+    subtitle: {
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        fontWeight: '500'
+    }
+});
+
+export default textStyles;
