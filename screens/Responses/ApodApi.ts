134e8b1ad24aa8131f7cf7fcf240dd283c4f5359
Breno Prestes <bcaua205@gmail.com>
2023-11-20 00:39:43 -0300

    Add api's responses

---
+++ screens/Responses/ApodApi.ts
@@ -0,0 +1,10 @@
+export default interface ApodApi {
+	concepts: string,
+	copyright: string,
+	date: string,
+	explanation: string,
+	hdurl: string,
+	media_type: string,
+	title: string,
+	url: string
+}
