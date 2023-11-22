e967be85bafda9cafd7cdd624debf119cc2107b1
Breno Prestes <bcaua205@gmail.com>
2023-11-22 18:35:01 -0300

    Add react-native-dotenv and types

---
+++ types/types.ts
@@ -0,0 +1,10 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import ApodApi from "../screens/Responses/ApodApi";
+
+export type RootStackParamList = {
+	Home: undefined;
+	Detail: { item: ApodApi }
+};
+
+export type PropsHome = NativeStackScreenProps<RootStackParamList, 'Home'>;
+export type PropsDetail = NativeStackScreenProps<RootStackParamList, 'Detail'>;
