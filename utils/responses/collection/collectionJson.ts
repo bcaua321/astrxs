d2680907d8a36dfc3f5a01add0f708ebc1701cf8
Breno Prestes <bcaua205@gmail.com>
2023-11-25 20:22:02 -0300

    Add Collection+JSON support

---
+++ utils/responses/collection/collectionJson.ts
@@ -0,0 +1,35 @@
+export interface collectionJson {
+    version: string,
+    href: string,
+    items: item[] 
+}
+
+export interface item {
+    href: string,
+    data:  itemDetail[],
+    links: link[]
+}
+
+interface itemDetail {
+    album: string[],
+    center: string,
+    title: string,
+    keywords: string[],
+    nasa_id: string,
+    date_created: Date,
+    media_type: string,
+    description: string
+}
+
+interface error {
+    title: string,
+    code: string,
+    message: string
+}
+
+interface link {
+    href: string,
+    rel: string,
+    prompt: string | undefined,
+    render: string | undefined
+}
--- utils/responses/nivlApi.ts
+++ utils/responses/nivlApi.ts
@@ -1,11 +1,6 @@
+import { collectionJson } from "./collection/collectionJson";
+
 // based on response of NASA Image and Video Library API
 export default interface NivlApi {
-	concepts: string,
-	copyright: string,
-	date: string,
-	explanation: string,
-	hdurl: string,
-	media_type: string,
-	title: string,
-	url: string
+	collection: collectionJson
 }
