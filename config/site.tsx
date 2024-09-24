0a432c10c360492f283f7c1b4338fdc29c08c660
Breno Prestes <bcaua205@gmail.com>
2024-09-23 23:09:10 -0300

    Add site config

---
+++ config/site.tsx
@@ -0,0 +1,14 @@
+export const siteConfig = {
+    name: "Astrxs",
+    description: "Astrxs is a platform for astronomy enthuasiats.",
+    navItems: [{
+        name: "Home",
+        href: "/"
+    }, {
+        name: "About",
+        href: "/about"
+    }, {
+        name: "Contact",
+        href: "/contact"
+    }]
+}
