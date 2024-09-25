d0ae66a8d63d83adfcae9abf8b0b7a1c2717a220
Breno Prestes <bcaua205@gmail.com>
2024-09-24 22:01:27 -0300

    Creating the Logo (Text)

---
+++ components/astrxsLogo.tsx
@@ -0,0 +1,9 @@
+const AstrxsLogo = () => {
+    return (
+        <span className="capitalize font-bold text-primary">
+            astr<span color="primary" className="text-foreground">x</span>s
+        </span>
+    )
+} 
+
+export default AstrxsLogo;
