90c09e7949abcbf229bfc9c4003039c9dbf26c9a
Breno Prestes <bcaua205@gmail.com>
2024-09-28 19:54:37 -0300

    add apod page and update gitignore

--- app/apod/page.tsx
+++ app/apod/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+
+import {  DatePicker, DateValue } from "@nextui-org/react";
+import { CalendarDate, getLocalTimeZone, today} from "@internationalized/date";
+import { useState } from "react";
+
+
 export default function Apod() {
+    const [selectedDate, setSelectedDate] = useState<CalendarDate | null>(null);
+    const dateMin = new CalendarDate(1995, 6, 16); // date of availability of the APOD API
+    
+    const handleDateChange = (date: CalendarDate | null ) => {
+        setSelectedDate(date);
+        console.log("Data selecionada:", date);
+    };
+
     return (
-        <p>Hello World.</p>
+        <>
+            <div>
+                <DatePicker 
+                    onChange={handleDateChange} 
+                    showMonthAndYearPickers  
+                    minValue={dateMin} 
+                    maxValue={today(getLocalTimeZone())} 
+                    label="Select a date" 
+                    className="max-w-[284px]" 
+                    defaultValue={today(getLocalTimeZone())}/>
+            </div>
+        </>
     )
 }
